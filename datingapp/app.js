4b4564ccf67c5ef13d12b04b3e5fe10c522d61b7
kilroy763 <itzmaxkiljan@hotmail.com>
2020-02-13 15:07:26 +0100

    Update app.js

--- datingapp/app.js
+++ datingapp/app.js
@@ -1,1 +1,15 @@
-console.log("Dit is een probeersel")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/about', function (req, res) {
+    res.send('doei World!')
+  })
+
+  app.get('/', function (req, res) {
+    throw new Error('BROKEN') // Express will catch this on its own.
+  })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
