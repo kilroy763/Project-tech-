64819fcb61bc9d8c8f7393195c6aa07eee5249ad
kilroy763 <itzmaxkiljan@hotmail.com>
2020-02-27 13:19:20 +0100

    query

--- datingapp/app.js
+++ datingapp/app.js
@@ -4,16 +4,21 @@ const port = 3000
 
 app.use(express.static('./public'));
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/about', function (req, res) {
     res.send('doei World!')
-  })
+  });
+
+  app.get('/eminem', function (req, res) {
+    res.sendFile ("/Users/maxmulder/muziek.m4a")
+  });
 
   app.get('*', (req, res) => {
         res.status(404).send("404 NOT FOUND");
   });
+
+
 
  
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
