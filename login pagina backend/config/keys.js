7d9c5155a1ef4865a74c6c5e836a2376a1e134ad
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-04-05 11:37:50 +0200

    Delete keys.js

--- login pagina backend/config/keys.js
+++
@@ -1,7 +0,0 @@
-dbPassword = "mongodb+srv://Kilroy763:TEST@projecttech-mt8t7.azure.mongodb.net/test?retryWrites=true&w=majority";
-
-module.exports = {
-    mongoURI: dbPassword
-};
-
-// link met de database
